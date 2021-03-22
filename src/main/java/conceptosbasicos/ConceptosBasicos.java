6f7760614b0c82ee6355ae2c39eb9c17de5d3e60
LESRLUEN <luisenriquesolisrocha@gmail.com>
2021-03-22 06:04:27 -0600

    Tarea Completar

--- src/main/java/conceptosbasicos/ConceptosBasicos.java
+++ src/main/java/conceptosbasicos/ConceptosBasicos.java
@@ -23,8 +23,37 @@ public class ConceptosBasicos {
         miBocho.setMarca("VW");
         miBocho.setSubMarca("Sedan");
         miBocho.setModelo(1970);
-        miBocho.setColor(Color.BLUE);
+        miBocho.setColor(Color.blue);
         System.out.println(miBocho);
+        
+         
+        //Tarea: Programar los objetos miAcura y miMustang
+        //Acura NSX
+        
+        Automovil miAcura = new Automovil();
+        miAcura.setMarca("Acura (Honda)");
+        miAcura.setSubMarca("Supercarro (NSX)");
+        miAcura.setModelo(2020);
+        miAcura.setColor(Color.white);
+        System.out.println(miAcura);
+        
+        //Mustang Shelby
+        
+        Automovil miMustang = new Automovil();
+        miMustang.setMarca("Ford Motor Company");
+        miMustang.setSubMarca("Carro deportivo (Mustang Shelby)");
+        miMustang.setModelo(1980);
+        miMustang.setColor(Color.red);
+        System.out.println(miMustang);
+        
+        // Nissan GTR
+        
+        Automovil miGTR = new Automovil();
+        miMustang.setMarca("Nissan");
+        miMustang.setSubMarca("Carro deportivo (GT-R)");
+        miMustang.setModelo(2022);
+        miMustang.setColor(Color.green);
+        System.out.println(miMustang);
     }
     
 }
